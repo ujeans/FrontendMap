45eaf08d2f07686e46f6ee83d06be5f29778b54b
ujeans <dbwlsxkal95@gmail.com>
2024-06-01 03:00:01 +0900

    라이브러리 이용해서 지도 불러오기 및 배경색 추가 #2

--- frontend/src/components/MapLayout.js
+++ frontend/src/components/MapLayout.js
@@ -1,7 +1,69 @@
 import React from "react";
+import styled from "styled-components";
+import { SVGMap } from "react-svg-map";
+import "react-svg-map/lib/index.css";
+import southKoreaMap from "@svg-maps/south-korea";
 
-const MapLayout = () => {
-  return <div>MapLayout</div>;
+const MapLayout = ({ selectedLocation, handleLocationClick }) => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>South Korea Map</Title>
+        <MapWrapper>
+          <SVGContainer>
+            <SVGMap map={southKoreaMap} onLocationClick={handleLocationClick} />
+          </SVGContainer>
+          {selectedLocation && (
+            <SelectedLocation>
+              Selected Location: {selectedLocation}
+            </SelectedLocation>
+          )}
+        </MapWrapper>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default MapLayout;
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #87ceeb; /* 하늘색 배경 추가 */
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.div`
+  margin-bottom: 80px;
+  font-size: 28px;
+  font-weight: 900;
+  color: white; /* 타이틀 글씨를 잘 보이게 하기 위해 */
+`;
+
+const MapWrapper = styled.div`
+  width: 30%;
+  z-index: 2;
+  position: relative;
+`;
+
+const SVGContainer = styled.div`
+  width: 100%;
+  height: auto;
+`;
+
+const SelectedLocation = styled.p`
+  margin-top: 10px;
+  color: white;
+  font-weight: bold;
+`;
--- frontend/src/pages/Map.js
+++ frontend/src/pages/Map.js
@@ -1,9 +1,5 @@
-import styled from "styled-components";
-import { useState } from "react";
-
-import { SVGMap } from "react-svg-map";
-import "react-svg-map/lib/index.css";
-import southKoreaMap from "@svg-maps/south-korea";
+import React, { useState } from "react";
+import MapLayout from "../components/MapLayout";
 
 const Map = () => {
   const [selectedLocation, setSelectedLocation] = useState(null);
@@ -13,42 +9,11 @@ const Map = () => {
   };
 
   return (
-    <Container>
-      <Wrapper>
-        <Title>South Korea Map</Title>
-        <MapWrapper>
-          <SVGContainer>
-            <SVGMap map={southKoreaMap} onLocationClick={handleLocationClick} />
-          </SVGContainer>
-          {selectedLocation && <p>Selected Location: {selectedLocation}</p>}
-        </MapWrapper>
-      </Wrapper>
-    </Container>
+    <MapLayout
+      selectedLocation={selectedLocation}
+      handleLocationClick={handleLocationClick}
+    />
   );
 };
 
 export default Map;
-
-const Container = styled.div`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Wrapper = styled.div``;
-
-const Title = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 70px;
-  font-size: 28px;
-  font-weight: 900;
-`;
-
-const MapWrapper = styled.div`
-  width: 400px;
-`;
-
-const SVGContainer = styled.div``;
